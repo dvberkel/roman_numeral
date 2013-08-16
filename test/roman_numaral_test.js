e9c879ef4066d515a29d95bdcd82640f99dc65c8
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-08-16 10:15:16 +0200

    create a roman function

---
+++ test/roman_numaral_test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var roman = require('../lib/roman_numeral');
+
+
+describe('roman', function(){
+    it('should be a function', function(){
+	assert.equal('function', typeof roman);
+    })
+});
