6e0ecd3f9323e44a45cc37e262c47c67067111af
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-08-16 10:21:48 +0200

    implement case 5

--- test/roman_numaral_test.js
+++ test/roman_numaral_test.js
@@ -11,5 +11,8 @@ describe('roman', function(){
 	it('should return \'I\' on 1', function(){
 	    assert.equal('I', roman(1));
 	})
+	it('should return \'V\' on 5', function(){
+	    assert.equal('V', roman(5));
+	})
     });
 });
